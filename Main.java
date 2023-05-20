41950a2bf7224338d864aa91fd58eac0487053ab
kijiya1995 <134154188+kijiya1995@users.noreply.github.com>
2023-05-20 23:15:55 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+package goboom.files;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Initialize Constructor
+        CardGame game = new CardGame();
+        // Start the game
+        game.startNewGame();
+    }
+
+}
